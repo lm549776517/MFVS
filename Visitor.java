6ce098f8444c26f8d44199ffe0171cf12a3218ab
MFVS <549776517@qq.com>
2018-09-26 16:59:34 +1000

    1

--- Visitor.java
+++ Visitor.java
@@ -16,10 +16,7 @@ public class Visitor extends User
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * 
      */
     public void register(String username, String password)
     {
